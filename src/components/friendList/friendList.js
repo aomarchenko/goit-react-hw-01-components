4c00bac93bc229c105a315ab36ab9563c9be1572
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-11-30 21:24:13 +0200

    up

--- src/components/friendList/friendList.js
+++ src/components/friendList/friendList.js
@@ -3,12 +3,15 @@ import styles from './FriendList.module.css';
 
 export default function FriendList(props) {
   const { friends } = props;
+  const status = [];
 
   return (
-    <ul className="friend-list">
+    <ul className={styles.friendList}>
       {friends.map(friend => (
-        <li key={friend.id} className="item">
-          <span className={friend.isOnline}></span>
+        <li key={friend.id} className={styles.item}>
+          <span className="green">""</span>
+          <span className={'' + (friend.isOnline ? 'green' : '')}></span>
+          {/* <span style={{ background: friend.isOnline ? 'green' : 'red' }}></span> */}
           <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
           <p class={friend.name}>{friend.name}</p>
         </li>
